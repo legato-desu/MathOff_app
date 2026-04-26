d558a3c71cb149cf5a6faab4a800f8bf17b96e8b
David Miranda <ivanchokr@gmail.com>
2026-04-26 04:12:15 -0500

    Desactivar sympy temporalmente

--- backend/ejercicios/views.py
+++ backend/ejercicios/views.py
@@ -1,22 +1,30 @@
-from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Ejercicio
 from .serializers import EjercicioSerializer
 
 
-class ListaEjerciciosView(generics.ListAPIView):
-    queryset = Ejercicio.objects.all()
-    serializer_class = EjercicioSerializer
+# CREAR ejercicio (docente)
+class CrearEjercicioView(APIView):
     permission_classes = [IsAuthenticated]
 
+    def post(self, request):
+        serializer = EjercicioSerializer(data=request.data)
 
-class CrearEjercicioView(generics.CreateAPIView):
-    queryset = Ejercicio.objects.all()
-    serializer_class = EjercicioSerializer
+        if serializer.is_valid():
+            serializer.save(creado_por=request.user)
+            return Response(serializer.data, status=201)
+
+        return Response(serializer.errors, status=400)
+
+
+# LISTAR ejercicios (estudiante)
+class ListarEjerciciosView(APIView):
     permission_classes = [IsAuthenticated]
 
-    def perform_create(self, serializer):
-        serializer.save(
-            creado_por=self.request.user
-        )
+    def get(self, request):
+        ejercicios = Ejercicio.objects.all().order_by("-id")
+        serializer = EjercicioSerializer(ejercicios, many=True)
+        return Response(serializer.data)
